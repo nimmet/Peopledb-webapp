9a9bfe7dd6bd9108c7eae87992dff95cce58cff0
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-05 15:50:31 +0200

    Spring app: save person to person repository

--- src/main/java/com/uyghur/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/uyghur/peopledbweb/web/controller/PeopleController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,9 +26,21 @@ public class PeopleController {
         return personRepository.findAll();
     }
 
+    @ModelAttribute
+    public Person getPerson(){
+        return new Person();
+    }
+
     @GetMapping
     public String showPeoplePage() {
 
         return "people";
+    }
+
+
+    @PostMapping
+    public String savePerson(Person person){
+        personRepository.save(person);
+        return "redirect:people";
     }
 }
