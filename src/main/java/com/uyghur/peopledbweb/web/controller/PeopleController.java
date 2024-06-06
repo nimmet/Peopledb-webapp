757587fbec935e8ae63fa9a6ff6a071a45cb8c81
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-06 15:32:54 +0200

    Spring app: delete people from list

--- src/main/java/com/uyghur/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/uyghur/peopledbweb/web/controller/PeopleController.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/people")
@@ -47,5 +47,13 @@ public class PeopleController {
             return "redirect:people";
         }
         return "people";
+    }
+
+    @PostMapping(params = "delete=true")
+    public String deletePeople(@RequestParam Optional<List<Long>> selections){
+        if (selections.isPresent()) {
+            personRepository.deleteAllById(selections.get());
+        }
+        return "redirect:people";
     }
 }
